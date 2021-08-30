579f5bc11304129cf8c9a81f2694a2323c30034a
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-08-30 11:31:38 -0300

    Aula Fluxo de Navegacao - Finalizada

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,22 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { ElementType } from "react";
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({icon, children, ...rest}: NavLinkProps) {
+export function NavLink({icon, children, href, ...rest}: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20"/>
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20"/>
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   )
 }
