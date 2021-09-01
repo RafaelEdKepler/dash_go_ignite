fabc201c37c86366b051ef0a79e7f74ebccb35f4
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-09-01 13:03:50 -0300

    Aula Listagem de Usuários - Finalizada

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,12 +9,23 @@ import { Sidebar } from "../../components/Sidebar";
 export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
-    const data = response.json();
+    const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    });
+
+    return users;
   });
-
-  console.log(data);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -66,32 +77,34 @@ export default function UserList() {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="pink"/>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Rafael Kepler</Text>
-                      <Text fontWeight="sm" color="gray.300">rafael.kepler@hotmail.com</Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && (
-                    <Td>
-                      04 de abril de 2021
+                {data.map(user => (
+                  <Tr key={user.id}>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink"/>
                     </Td>
-                  )}
-                  <Td>
-                    {isWideVersion ? (
-                      <Button as="a" size="sm" fontSize="16" colorScheme="purple" leftIcon={<Icon as={RiPencilLine}/>}>
-                        Editar
-                      </Button>
-                    ) : (
-                      <IconButton aria-label="Edit user" as="a" size="sm" fontSize="16" colorScheme="purple" icon={<Icon as={RiPencilLine}/>}/>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">{user.name}</Text>
+                        <Text fontWeight="sm" color="gray.300">{user.email}</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && (
+                      <Td>
+                        {user.createdAt}
+                      </Td>
                     )}
+                    <Td>
+                      {isWideVersion ? (
+                        <Button as="a" size="sm" fontSize="16" colorScheme="purple" leftIcon={<Icon as={RiPencilLine}/>}>
+                          Editar
+                        </Button>
+                      ) : (
+                        <IconButton aria-label="Edit user" as="a" size="sm" fontSize="16" colorScheme="purple" icon={<Icon as={RiPencilLine}/>}/>
+                      )}
 
-                  </Td>
-                </Tr>
+                    </Td>
+                  </Tr>
+                ))}
               </Tbody>
             </Table>
             <Pagination/>
